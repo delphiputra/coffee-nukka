f7e8da81b61a5e0f49c1540d95953e964296b472
Arieerdanta <ariere611@gmail.com>
2025-01-05 21:58:57 +0700

    arie: membuat page manajemen user

--- app/admin/user/page.tsx
+++ app/admin/user/page.tsx
@@ -1,1 +1,9 @@
-export default function CoffeeMenu() {}
+export default function UserPage() {
+  return (
+    <div className="p-6 bg-gradient-to-r from-gray-100 to-gray-200 shadow-lg rounded-lg">
+      <h1 className="text-4xl font-extrabold text-gray-700 mb-6 text-center">
+        👥 Manajemen Pengguna
+      </h1>
+    </div>
+  );
+}
