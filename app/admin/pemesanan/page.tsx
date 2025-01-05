f5de279507b3e3ca3cce46ae1ea81ff76922da6b
delphi <delphiputtera@example.com>
2025-01-05 22:13:34 +0700

    delphi: membuat tampilan pemesanan

--- app/admin/pemesanan/page.tsx
+++ app/admin/pemesanan/page.tsx
@@ -1,1 +1,9 @@
-export default function CoffeeMenu() {}
+export default function PemesananPage() {
+    return (
+      <div className="bg-blue-100 p-6 rounded-lg">
+        <h1 className="text-4xl font-bold text-blue-600">Pemesanan</h1>
+        <p className="mt-4 text-gray-800">Manage orders here.</p>
+      </div>
+    );
+  }
+  
