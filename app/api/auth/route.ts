1bd74695fc9528978336afc4258792e998e261bd
delphiputra <delphiputtera@gmail.com>
2025-01-16 23:54:18 +0700

    delphi: menghapus password ketika sudah di login

--- app/api/auth/route.ts
+++ app/api/auth/route.ts
@@ -31,3 +31,16 @@ export async function POST(req: Request): Promise<Response> {
         { status: 401 }
       );
     }
+
+    // Hapus password sebelum mengirim respons
+    const { password: _, ...userData } = user;
+
+    return new Response(JSON.stringify(userData), { status: 200 });
+  } catch (error: any) {
+    console.error("Error in login:", error.message);
+    return new Response(
+      JSON.stringify({ error: "Terjadi kesalahan pada server" }),
+      { status: 500 }
+    );
+  }
+}
