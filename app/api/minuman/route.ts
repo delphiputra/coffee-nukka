4d874d3e9e27564660511ed104c9abcd0951600f
delphiputra <delphiputtera@gmail.com>
2025-01-20 21:23:45 +0700

    delphi : fungsi POST ditambahkan untuk memungkinkan penambahan data baru ke tabel minuman.

--- app/api/minuman/route.ts
+++ app/api/minuman/route.ts
@@ -2,10 +2,34 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
+interface MinumanRequestBody {
+  name: string;
+  price: string;
+}
+
+interface Request {
+  json: () => Promise<MinumanRequestBody>;
+}
+
 export async function GET(req: Request): Promise<Response> {
   try {
     const minuman = await prisma.minuman.findMany();
     return new Response(JSON.stringify(minuman), { status: 200 });
+  } catch (error: any) {
+    return new Response(JSON.stringify({ error: error.message }), { status: 500 });
+  }
+}
+
+export async function POST(req: Request): Promise<Response> {
+  try {
+    const body = await req.json();
+    const { name, price } = body;
+
+    const newMinuman = await prisma.minuman.create({
+      data: { name, price: parseFloat(price) },
+    });
+
+    return new Response(JSON.stringify(newMinuman), { status: 201 });
   } catch (error: any) {
     return new Response(JSON.stringify({ error: error.message }), { status: 500 });
   }
